defb21a88dd6f784ed0576c90d6eb5997a84b313
Patrick Steiner <psteiner@redhat.com>
2016-05-17 15:07:33 +0200

    fixed URLs

--- runRulesService.sh
+++ runRulesService.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export SOURCE_AMQ_BROKER="tcp://receiver:61616"
+export SOURCE_AMQ_BROKER="tcp://localhost:61616"
 export SOURCE_QUEUE="message.to.rules"
 export SOURCE_BROKER_ADMIN_UID="admin"
 export SOURCE_BROKER_ADMIN_PASSWD="change12_me"
-export TARGET_AMQ_BROKER="tcp://receiver:61616"
+export TARGET_AMQ_BROKER="tcp://localhost:61616"
 export TARGET_QUEUE="message.to.datacenter"
 export TARGET_BROKER_ADMIN_UID="admin"
 export TARGET_BROKER_ADMIN_PASSWD="change12_me"
