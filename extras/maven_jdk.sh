712aaac0d89b47385f637881d60b3254b1894fb0
ishuverma <iverma@redhat.com>
2016-08-29 15:03:52 -0700

    Create maven_jdk.sh

---
+++ extras/maven_jdk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MAVEN_HOME=/opt/apache-maven-3.3.9
+PATH=$MAVEN_HOME/bin:$PATH
+JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk
+M2_HOME=/opt/apache-maven-3.3.9/
+export PATH MAVEN_HOME JAVA_HOME M2_HOME
+export CLASSPATH=.
